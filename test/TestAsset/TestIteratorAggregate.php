9d6697001a4cdeb91af0e4eac9407dbb6223e5ff
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:13:56 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/TestIteratorAggregate.php
+++ test/TestAsset/TestIteratorAggregate.php
@@ -1,17 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Json
+ * @see       https://github.com/laminas/laminas-json for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-json/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-json/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Json\TestAsset;
+namespace LaminasTest\Json\TestAsset;
 
 /**
- * @see ZF-12347
+ * @see Laminas-12347
  */
 class TestIteratorAggregate implements \IteratorAggregate
 {
