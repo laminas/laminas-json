eaa66690fda0622c3ef598940d73518de3cda41a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:13:47 -0600

    Rewriting as Laminas Project package

--- test/Server/CacheTest.php
+++ test/Server/CacheTest.php
@@ -1,25 +1,23 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Json
+ * @see       https://github.com/laminas/laminas-json for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-json/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-json/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Json\Server;
+namespace LaminasTest\Json\Server;
 
-use Zend\Json\Server;
+use Laminas\Json\Server;
 
 /**
- * Test class for Zend_JSON_Server_Cache
+ * Test class for Laminas_JSON_Server_Cache
  *
- * @category   Zend
- * @package    Zend_JSON_Server
+ * @category   Laminas
+ * @package    Laminas_JSON_Server
  * @subpackage UnitTests
- * @group      Zend_JSON
- * @group      Zend_JSON_Server
+ * @group      Laminas_JSON
+ * @group      Laminas_JSON_Server
  */
 class CacheTest extends \PHPUnit_Framework_TestCase
 {
@@ -33,7 +31,7 @@ class CacheTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->server = new Server\Server();
-        $this->server->setClass('ZendTest\Json\Server\Foo', 'foo');
+        $this->server->setClass('LaminasTest\Json\Server\Foo', 'foo');
         $this->cacheFile = tempnam(sys_get_temp_dir(), 'zjs');
 
         // if (!is_writeable(__DIR__)) {
