273d97e0946b19fcf495cd0757f59a56c534e652
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:13:39 -0600

    Rewriting as Laminas Project package

--- src/Server/Request/Http.php
+++ src/Server/Request/Http.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Json
+ * @see       https://github.com/laminas/laminas-json for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-json/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-json/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Json\Server\Request;
+namespace Laminas\Json\Server\Request;
 
-use Zend\Json\Server\Request as JsonRequest;
+use Laminas\Json\Server\Request as JsonRequest;
 
 /**
- * @category   Zend
- * @package    Zend_Json
+ * @category   Laminas
+ * @package    Laminas_Json
  */
 class Http extends JsonRequest
 {
