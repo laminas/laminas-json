656a8b0093142e08789cae81752f038a3edecc63
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:07:29 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/TestAsset/ToJSONWithExpr.php
+++ test/TestAsset/ToJSONWithExpr.php
@@ -1,10 +1,4 @@
 <?php // @codingStandardsIgnoreFile
-/**
- * @see       https://github.com/laminas/laminas-json for the canonical source repository
- * @copyright https://github.com/laminas/laminas-json/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-json/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\Json\TestAsset;
 
 use Laminas\Json\Expr;
